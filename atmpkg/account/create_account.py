622f0caaec75a3609046a4ad063c2828fcc7a531
Simón Maquilón <simon.maquilon@minusjob.com>
2023-08-15 00:11:18 -0500

    Estudiar código base antes de iniciar

---
+++ atmpkg/account/create_account.py
@@ -0,0 +1,17 @@
+
+
+def register_customer(nombre, apellido, identificacion, correo):
+    """
+    Register a Customer
+
+    Args:
+        nombre (_type_): _description_
+        apellido (_type_): _description_
+        identificacion (_type_): _description_
+        correo (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    print("Create Account")
+    return "simon"
